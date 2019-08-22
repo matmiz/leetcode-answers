f67a2965d57fc37bf70771e6c37a69a79773ad92
matan.mizrahi <matmiz1@gmail.com>
2019-08-22 14:00:50 +0300

    27 - Remove Element

---
+++ src/easy/removeElement.js
@@ -0,0 +1,16 @@
+/**
+ *
+ * runtime O(n)
+ * space O(1);
+ * 
+ */
+var removeElement = function(nums, val) {
+    let j = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== val) {
+            nums[j] = nums[i];
+            j++;
+        }
+    }
+    return j;
+};
